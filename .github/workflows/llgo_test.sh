3fca7a7ba7a322c9fd770fa5439ad5c8a5b2ce84
Haolan <coopticks@outlook.com>
2025-05-23 18:23:58 +0800

    fix: use llgo test . directly

--- .github/workflows/llgo_test.sh
+++ .github/workflows/llgo_test.sh
@@ -6,8 +6,7 @@ root_dir=$(pwd)
 for dir in "$@"; do
     echo "Testing $dir"
     cd "$root_dir/$dir"
-    output=$(llgo test . 2>&1)
-    echo "$output"
+    llgo test .
 done
 
 echo "All tests passed!"
