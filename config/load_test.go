bf93c77434a0d1630ca8acc72f171fef87d8b03b
Haolan <coopticks@outlook.com>
2025-05-27 18:02:19 +0800

    test: add test for config stdin

--- config/load_test.go
+++ config/load_test.go
@@ -1,22 +1,31 @@
 package config_test
 
 import (
+	"fmt"
+	"os"
 	"reflect"
-	"strings"
 	"testing"
 
 	llconfig "github.com/goplus/llcppg/config"
+)
+
+type testMode int
+
+const (
+	useStdin testMode = 1 << iota
+	useFile
 )
 
 func TestGetConfByByte(t *testing.T) {
 	testCases := []struct {
 		name      string
 		input     string
+		mode      testMode
 		expect    llconfig.Config
 		expectErr bool
 	}{
 		{
-			name: "SQLite configuration",
+			name: "SQLite configuration(File)",
 			input: `{
   "name": "sqlite",
   "cflags": "-I/opt/homebrew/opt/sqlite/include",
@@ -39,10 +48,38 @@ func TestGetConfByByte(t *testing.T) {
 					"sqlite3_finalize": ".Close",
 				},
 			},
+			mode: useFile,
 		},
 
 		{
-			name: "Lua configuration",
+			name: "SQLite configuration(Stdin)",
+			input: `{
+  "name": "sqlite",
+  "cflags": "-I/opt/homebrew/opt/sqlite/include",
+  "include": ["sqlite3.h"],
+  "libs": "-L/opt/homebrew/opt/sqlite/lib -lsqlite3",
+  "trimPrefixes": ["sqlite3_"],
+  "cplusplus": false,
+  "symMap": {
+    "sqlite3_finalize":".Close"
+  }
+}`,
+			expect: llconfig.Config{
+				Name:         "sqlite",
+				CFlags:       "-I/opt/homebrew/opt/sqlite/include",
+				Include:      []string{"sqlite3.h"},
+				Libs:         "-L/opt/homebrew/opt/sqlite/lib -lsqlite3",
+				TrimPrefixes: []string{"sqlite3_"},
+				Cplusplus:    false,
+				SymMap: map[string]string{
+					"sqlite3_finalize": ".Close",
+				},
+			},
+			mode: useStdin,
+		},
+
+		{
+			name: "Lua configuration(file)",
 			input: `{
 		  "name": "lua",
 		  "cflags": "-I/opt/homebrew/include/lua",
@@ -58,17 +95,78 @@ func TestGetConfByByte(t *testing.T) {
 				Libs:         "-L/opt/homebrew/lib -llua -lm",
 				TrimPrefixes: []string{"lua_", "lua_"},
 			},
+			mode: useFile,
 		},
+
+		{
+			name: "Lua configuration(stdin)",
+			input: `{
+		  "name": "lua",
+		  "cflags": "-I/opt/homebrew/include/lua",
+		  "include": ["lua.h"],
+		  "libs": "-L/opt/homebrew/lib -llua -lm",
+		  "trimPrefixes": ["lua_", "lua_"],
+		  "cplusplus": false
+		}`,
+			expect: llconfig.Config{
+				Name:         "lua",
+				CFlags:       "-I/opt/homebrew/include/lua",
+				Include:      []string{"lua.h"},
+				Libs:         "-L/opt/homebrew/lib -llua -lm",
+				TrimPrefixes: []string{"lua_", "lua_"},
+			},
+			mode: useStdin,
+		},
+
 		{
 			name:      "Invalid JSON",
 			input:     `{invalid json}`,
 			expectErr: true,
+			mode:      useStdin,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			result, err := llconfig.ConfigFromReader(strings.NewReader(tc.input))
+			var result llconfig.Config
+
+			file, err := os.CreateTemp("", "configtest")
+			if err != nil {
+				t.Fatal(err)
+				return
+			}
+			defer os.Remove(file.Name())
+
+			_, err = file.Write([]byte(tc.input))
+			if err != nil {
+				t.Fatal(err)
+				return
+			}
+			err = fmt.Errorf("config: no mode is specified")
+
+			if tc.mode&useStdin != 0 {
+				var fileR *os.File
+				stdin := os.Stdin
+				defer func() { os.Stdin = stdin }()
+
+				//	Seek isn't implemented in llgo, so we have to open the file again to reset the cursor
+				fileR, err = os.Open(file.Name())
+				if err != nil {
+					t.Fatal(err)
+					return
+				}
+				defer fileR.Close()
+
+				// swap for testing
+				os.Stdin = fileR
+
+				result, err = llconfig.GetConfFromStdin()
+			}
+
+			if tc.mode&useFile != 0 {
+				result, err = llconfig.GetConfFromFile(file.Name())
+			}
+
 			if tc.expectErr {
 				if err == nil {
 					t.Fatalf("expected error for test case %s, but got nil", tc.name)
