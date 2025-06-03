d78acea49f731f96c925f424880966eaf6d8559c
Haolan <coopticks@outlook.com>
2025-06-03 13:45:10 +0800

    style: fix tab

--- cmd/llcppgtest/demo/demo.go
+++ cmd/llcppgtest/demo/demo.go
@@ -52,7 +52,7 @@ func logFile(demoDir string) (*os.File, error) {
 // _llcppgtest/lua/           - Demo root directory
 //
 //		├── llcppg.cfg          - LLCPPG configuration file
-//	 ├── conf         		- Configuration directory for speficied platforms
+//	 	├── conf         		- Configuration directory for speficied platforms
 //		|	└── linux			- Linux Platform
 //		| 	  	 └── llcppg.cfg - LLCPPG configuration file (linux platform)
 //		├── out/                - Generated package output directory
