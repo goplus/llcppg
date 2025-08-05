dcb330a3cee77817dd927dcdb45466c10147f0fb
Haolan <hello@liuxi.ng>
2025-08-05 14:15:24 +0800

    test: revert struct test in parser

--- _xtool/internal/parser/testdata/struct/temp.h
+++ _xtool/internal/parser/testdata/struct/temp.h
@@ -1,50 +1,25 @@
-struct NestedEnum
-{
-    enum
-    {
-        APR_BUCKET_DATA1 = 0,
-        APR_BUCKET_METADATA2 = 1
-    } is_metadata1_t;
-
-    struct a
-    {
-        int b;
-    } a_t;
+struct {
+    int a;
 };
 
-struct NestedEnum2
-{
-    enum
-    {
-        APR_BUCKET_DATA3 = 0,
-        APR_BUCKET_METADATA4 = 1
-    };
+struct Foo1 {
+    int a;
+    int b;
+};
+struct Foo2 {
+    int a, b;
 };
 
-struct NestedEnum3
-{
-    enum is_metadata3
-    {
-        APR_BUCKET_DATA5 = 0,
-        APR_BUCKET_METADATA6 = 1
-    };
+struct Foo3 {
+    int a;
+    int (*Foo)(int, int);
 };
 
-struct NestedEnum4
-{
-    enum is_metadata4
-    {
-        APR_BUCKET_DATA7 = 0,
-        APR_BUCKET_METADATA8 = 1
-    } key;
-};
-
-enum OuterEnum
-{
-    APR_BUCKET_DATA9 = 0,
-    APR_BUCKET_METADATA10 = 1
-};
-struct Enum
-{
-    enum OuterEnum k;
+struct Person {
+    int age;
+    struct {
+        int year;
+        int day;
+        int month;
+    } birthday;
 };
