e0fb166b889c738ee0fbad1d357fbdfb14b060de
luoliwoshang <2643523683@qq.com>
2025-08-21 15:58:11 +0800

    abi=0

--- .github/workflows/test_demo.sh
+++ .github/workflows/test_demo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# llgo run subdirectories under _demo and _pydemo that contain *.go files
+# llgo run -abi=0 subdirectories under _demo and _pydemo that contain *.go files
 total=0
 failed=0
 failed_cases=""
@@ -9,8 +9,8 @@ for d in ./_demo/* ./_pydemo/*; do
   if [ -d "$d" ] && [ -n "$(ls "$d"/*.go 2>/dev/null)" ]; then
     total=$((total+1))
     echo "Testing $d"
-    if ! (cd "$d" && llgo run .); then
-      if ! (go mod tidy && llgo run .); then
+    if ! (cd "$d" && llgo run -abi=0 .); then
+      if ! (go mod tidy && llgo run -abi=0 .); then
         echo "FAIL"
         failed=$((failed+1))
         failed_cases="$failed_cases\n* :x: $d"
