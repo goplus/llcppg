2227d0eed58e988291e8fda3b6c11c67c389bba3
luoliwoshang <2643523683@qq.com>
2025-08-21 16:44:32 +0800

    llgo:component -abi=0

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ go install -v ./cmd/llcppcfg
 go install -v ./cmd/llcppgtest
 
 # main process required
-llgo install ./_xtool/llcppsymg
-llgo install ./_xtool/llcppsigfetch
+llgo install -abi=0 ./_xtool/llcppsymg
+llgo install -abi=0 ./_xtool/llcppsigfetch
 go install -v ./cmd/gogensig
 go install -v ./cmd/llcppg
