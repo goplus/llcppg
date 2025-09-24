ec6f297effb6cb8c92922c1adfd52b5426bd06a6
Haolan <hello@liuxi.ng>
2025-09-24 10:00:53 +0800

    revert temp.h

--- _xtool/internal/parser/testdata/enum/temp.h
+++ _xtool/internal/parser/testdata/enum/temp.h
@@ -1,24 +1,20 @@
-enum
-{
+enum {
     a,
     b,
     c,
 };
 
-enum Foo1
-{
+enum Foo1 {
     Foo1a,
     Foo1b,
     Foo1c,
 };
-enum Foo2
-{
+enum Foo2 {
     Foo2a = 1,
     Foo2b = 2,
     Foo2c = 4,
 };
-enum Foo3
-{
+enum Foo3 {
     Foo3a = 1,
     Foo3b,
     Foo3c,
