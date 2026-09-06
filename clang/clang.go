58f0b026daf457814cc612619d9b0a1c56b9f65c
xushiwei <x@goplus.org>
2026-09-06 19:52:55 +0800

    cmd: llcppdump

--- clang/clang.go
+++ clang/clang.go
@@ -34,7 +34,7 @@ type Index struct {
 }
 
 /**
- * CreateIndex provides a shared context for creating translation units.
+ * Provides a shared context for creating translation units.
  *
  * It provides two options:
  *
@@ -89,6 +89,73 @@ type TranslationUnit struct {
  */
 func (u TranslationUnit) Dispose() {
 	u.impl.Dispose()
+}
+
+/**
+ * Retrieve the cursor that represents the given translation unit.
+ *
+ * The translation unit cursor can be used to start traversing the
+ * various declarations within the given translation unit.
+ */
+func (u TranslationUnit) Cursor() Cursor {
+	return u.impl.Cursor()
+}
+
+// -----------------------------------------------------------------------------
+
+/**
+ * A cursor representing some element in the abstract syntax tree for
+ * a translation unit.
+ *
+ * The cursor abstraction unifies the different kinds of entities in a
+ * program--declaration, statements, expressions, references to declarations,
+ * etc.--under a single "cursor" abstraction with a common set of operations.
+ * Common operation for a cursor include: getting the physical location in
+ * a source file where the cursor points, getting the name associated with a
+ * cursor, and retrieving cursors for any child nodes of a particular cursor.
+ *
+ * Cursors can be produced in two specific ways.
+ * clang_getTranslationUnitCursor() produces a cursor for a translation unit,
+ * from which one can use clang_visitChildren() to explore the rest of the
+ * translation unit. clang_getCursor() maps from a physical source location
+ * to the entity that resides at that location, allowing one to map from the
+ * source code into the AST.
+ */
+type Cursor = clang.Cursor
+
+/**
+ * Describes how the traversal of the children of a particular
+ * cursor should proceed after visiting a particular child cursor.
+ */
+type ChildVisitResult = clang.ChildVisitResult
+
+const (
+	/**
+	 * Terminates the cursor traversal.
+	 */
+	Break ChildVisitResult = clang.ChildVisit_Break
+
+	/**
+	 * Continues the cursor traversal with the next sibling of
+	 * the cursor just visited, without visiting its children.
+	 */
+	Continue ChildVisitResult = clang.ChildVisit_Continue
+
+	/**
+	 * Recursively traverse the children of this cursor, using
+	 * the same visitor and client data.
+	 */
+	Recurse ChildVisitResult = clang.ChildVisit_Recurse
+)
+
+// VisitChildren visits the children of a particular cursor, invoking the given
+// visitor function for each child cursor. The traversal may be recursive,
+// depending on the return value of the visitor function.
+func VisitChildren(root Cursor, fn func(cur, parent Cursor) ChildVisitResult) uint {
+	return uint(clang.VisitChildren(
+		root, func(cur, parent Cursor, param clang.ClientData) ChildVisitResult {
+			return c.GoClosure[func(cur, parent Cursor) ChildVisitResult](param)(cur, parent)
+		}, c.ClosureData(fn)))
 }
 
 // -----------------------------------------------------------------------------
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/goplus/llcppg
 
 go 1.27.0
 
-require github.com/goplus/lib v0.5.1
+require github.com/goplus/lib v0.5.2
